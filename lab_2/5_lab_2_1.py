dae1265041af3461f7f82a8d91bd4f1c84bfb6c1
Krystalxd <61287173+Krystalxd@users.noreply.github.com>
2020-03-26 13:59:28 +0300

    Update 5_lab_2_1.py

--- lab_2/5_lab_2_1.py
+++ lab_2/5_lab_2_1.py
@@ -4,112 +4,97 @@ import math
 def sqrt_decomposition():
     while True:
         n = input('Введите размерность массива (P.S. Для выхода из программы введите слово "exit"): ')
-        if n == "exit":  # Если пользователь захочет закончить выполнение программы, то он может ввести слово "exit"
+        if n == "exit":  # Type "exit" to exit from the programme
             exit()
-        while n.isalpha() or int(n) < 0:  # Проверка корректности ввода данных (Размер массива-неотрицательное, 
-            # целое число) 
+        while n.isalpha() or int(n) < 0:  # Validation of input data
             n = input("Введите размерность массива: ")
         n = int(n)
 
         arr = []
-        for i in range(n):  # Цикл для заполнения массива 
+        for i in range(n):
             arr.append(int(input("Введите элемент массива: ")))
         print(arr)
 
-        b = input(
-            "С какого элемента нужно найти сумму (P.S. Учитывайте, что нумерация элементов массива начинается с 0): ")
-        if b == "exit":
+        l, r = input("Введите левую, а затем правую границу суммы: ").split()
+        if l == "exit" or r == "exit":
             exit()
-        while b.isalpha() or int(b) >= n or int(b) < 0:  # Проверка корректности ввода данных 
-            b = input("С какого элемента нужно найти сумму (P.S. Учитывайте, что нумерация элементов массива "
-                      "начинается с "
-                      "0): ")
-        b = int(b)
-
-        c = input("До какого элемента (P.S. Учитывайте, что нумерация элементов массива начинается с 0): ")
-        if c == "exit":
-            exit()
-        while c.isalpha() or int(c) < 0 or int(c) >= n:  # Проверка корректности ввода данных 
-            c = input("До какого элемента (P.S. Учитывайте, что нумерация элементов массива начинается с 0): ")
-        c = int(c)
-        length = int(math.sqrt(n)) + 1  # Длина одного блока 
-        z = [0] * length  # Массив для сумм
-        n_b = b // length  # Номер блока, которому принадлежит нижняя граница 
-        n_c = c // length  # Номер блока, которому принадлежит верхняя граница 
-        result = 0  # Конечная сумма
+        while l.isalpha() or r.isalpha() or int(l) < 0 or int(r) < 0 or int(l) > int(r) or int(l) >= int(n) or \
+                int(r) >= int(n):
+            l = int(l)
+            r = int(r)
+        l, r = int(l), int(r)
+        length = int(math.sqrt(n)) + 1
+        sum = [0] * length  # array for sum of subsections
+        n_l, n_r = l // length, r // length  # block with lower and upper bound
+        result = 0  # Final sum
 
         i = 0
-        while i < n:  # Заполняем массив "z" суммами наших блоков 
-            z[i // length] += arr[i] 
+        while i < n:  # Filling array "sum" with the sum of subsections
+            sum[i // length] += arr[i]
             i += 1
 
-        if n_b == n_c:  # Случай, когда b и c в одном блоке 
-            i = b
-            while i <= c:
+        if n_l == n_r:  # Case for l and r in the same subsection
+            i = l
+            while i <= r:
                 result += arr[i]
                 i += 1
         else:
-            i = b
-            while i < (n_b + 1) * length:  # Цикл для обработки левой границы множества, 
-                result += arr[i]  # невходящей полностью в один из блоков 
+            i = l
+            while i < (n_l + 1) * length:  # Loop for checking the left bound of the quantity
+                result += arr[i]
                 i += 1
 
-            i = n_b + 1
-            while i < n_c:  # Если часть множество принадлежит одному из блоков 
-                result += z[i]
+            i = n_l + 1
+            while i < n_r:  # If the part of the quantity belongs to any subsection
+                result += sum[i]
                 i += 1
 
-            i = n_c * length
-            while i <= c:  # Цикл для обработки правой границы множества, 
-                result += arr[i]  # невходящей полностью в один из блоков 
+            i = n_r * length
+            while i <= r:  # Loop for checking the right bound of the quantity
+                result += arr[i]
                 i += 1
-        print("Сумма элементов с ", b, "по", c, "равна: ", result)
+        print("Сумма элементов с ", l, "по", r, "равна: ", result)
 
 
-def sqrt_from_file():  # Функция для чтения информации из файла 
-    file = open("Считываемые данные.txt", "r")
-    info = file.read()
-    info = info.replace("[", " ")  # Заменяет квадратные скобки пробелами для дальнейшей работы 
-    info = info.replace("]", " ")
-    info = info.replace("\n", " ")  # Замена переходов на новую строку пробелами 
-    info = info.replace(",", " ")  # Замена запятых на пробелы 
-    info = info.split()
-    array = info[:-2]
-    amount = len(array)
-    length = int(math.sqrt(amount)) + 1  # Длина одного блока 
-    z = [0] * length  # Массив для сумм
-    b = int(info[-2])
-    c = int(info[-1])
-    n_b = b // length  # Номер блока, которому принадлежит нижняя граница 
-    n_c = c // length  # Номер блока, которому принадлежит верхняя граница 
-    result = 0  # Конечная сумма
+def sqrt_from_file():
+    with open("Считываемые данные.txt") as file:
+        info = file.read()
+        info = info.replace("\n", " ")
+        info = info.split()
+        array = info[:-2]
+        amount = len(array)
+        length = int(math.sqrt(amount)) + 1
+        sum = [0] * length
+        l, r = int(info[-2]), int(info[-1])
+        n_l, n_r = l // length, r // length
+        result = 0
 
-    i = 0
-    while i < len(array):  # Заполняем массив "z" суммами наших блоков 
-        z[i // length] += int(array[i])
-        i += 1
-
-    if n_b == n_c:  # Случай, когда b и c в одном блоке 
-        i = b
-        while i <= c:
-            result += int(array[i])
-            i += 1
-    else:
-        i = b
-        while i < (n_b + 1) * length:  # Цикл для обработки левой границы множества, 
-            result += int(array[i])  # невходящей полностью в один из блоков 
+        i = 0
+        while i < len(array):
+            sum[i // length] += int(array[i])
             i += 1
 
-        i = n_b + 1
-        while i < n_c:  # Если часть множество принадлежит одному из блоков 
-            result += z[i]
-            i += 1
+        if n_l == n_r:
+            i = l
+            while i <= r:
+                result += int(array[i])
+                i += 1
+        else:
+            i = l
+            while i < (n_l + 1) * length:
+                result += int(array[i])
+                i += 1
 
-        i = n_c * length
-        while i <= c:  # Цикл для обработки правой границы множества, 
-            result += int(array[i])  # невходящей полностью в один из блоков 
-            i += 1
-    print("Нужная вам сумма: ", result)
+            i = n_l + 1
+            while i < n_r:
+                result += sum[i]
+                i += 1
+
+            i = n_r * length
+            while i <= r:
+                result += int(array[i])
+                i += 1
+        print("Нужная вам сумма: ", result)
 
 
-sqrt_from_file()
+sqrt_decomposition()
