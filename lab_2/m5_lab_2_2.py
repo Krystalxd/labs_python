0efb6bf1836743959f620b927c5210a7e0a0355d
Krystalxd <61287173+Krystalxd@users.noreply.github.com>
2020-03-29 17:07:47 +0300

    Update m5_lab_2_2.py

--- lab_2/m5_lab_2_2.py
+++ lab_2/m5_lab_2_2.py
@@ -5,32 +5,30 @@ import argparse
 parser = argparse.ArgumentParser(description="Spamming file")  # Creating of parser to use with command line
 parser.add_argument("-mb", "--mb", type=int, required=True, help="Size of the file")
 parser.add_argument("-file", "--file", type=str, required=True, help="Name of the file")
-parser.add_argument("-word_tuple_str", "--word_tuple_str", type=str, default=(3, 10), help="Amount of word symbols")
-parser.add_argument("-line_tuple_str", "--line_tuple_str", type=str, default=(10, 100), help="Size of the file")
+parser.add_argument("-word_tuple_str", "--word_tuple_str", type=str, default="3,10", help="Amount of word symbols")
+parser.add_argument("-line_tuple_str", "--line_tuple_str", type=str, default="10,100", help="Size of the file")
 args = parser.parse_args()
 
 
 def filespam():
     try:
         while True:
-            mb, name = input("Введите размер файла и его название через символ ',' : ").split(",")
+            mb, name = input("Введите размер файла и его название через символ '/' : ").split("/")
             if mb == "exit" or name == "exit":
                 break  # Type "exit" to stop programme
             while mb.isalpha() or int(mb) < 0 or name[-4:] == ".txt":
-                mb, name = input("Введите размер файла и его название через символ ',' : ").split(",")
+                mb, name = input("Введите размер файла и его название через символ '/' : ").split("/")
             mb, name = int(mb), str(name)
             file = name + ".txt"
 
             word_tuple_str, line_tuple_str = input("Введите кол-во букв в слове и кол-во слов в строке (введите 0,0 для"
-                                                   " значения по умолчанию) через знак ',': ").split(",")
+                                                   " значения по умолчанию) через знак '/': ").split("/")
             if word_tuple_str == "exit" or line_tuple_str == "exit":
                 break
             if word_tuple_str == "0,0":
                 word_tuple_str = "3,10"
             if line_tuple_str == "0,0":
                 line_tuple_str = "10,100"
-            word_tuple, line_tuple = tuple(map(int, word_tuple_str.split(","))), \
-                                     tuple(map(int, line_tuple_str.split(",")))
             while word_tuple_str.split(",")[0].isalpha() or int(word_tuple_str.split(",")[0]) < 0 or \
                     word_tuple_str.split(",")[1].isalpha() or int(word_tuple_str.split(",")[1]) < 0 or \
                     line_tuple_str.split(",")[0].isalpha() or int(line_tuple_str.split(",")[0]) < 0 or \
@@ -38,7 +36,7 @@ def filespam():
                     int(word_tuple_str.split(",")[0]) > int(word_tuple_str.split(",")[1]) or \
                     int(line_tuple_str.split(",")[0]) > int(line_tuple_str.split(",")[1]):  # Validation of input data
                 word_tuple_str, line_tuple_str = input("Введите кол-во букв в слове и кол-во слов в строке "
-                                                       "(введите 0,0 для значения по умолчанию) через знак ',': ")\
+                                                       "(введите 0,0 для значения по умолчанию) через знак ',': ") \
                     .split(",")
             word_tuple, line_tuple = tuple(map(int, word_tuple_str.split(","))), \
                                      tuple(map(int, line_tuple_str.split(",")))
