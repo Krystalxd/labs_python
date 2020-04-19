e906c2e8484f6b660702e84c9e44b51ebefa44dd
Krystalxd <61287173+Krystalxd@users.noreply.github.com>
2020-04-19 16:00:17 +0300

    Update m5_lab_2_4.py

--- lab_2/m5_lab_2_4.py
+++ lab_2/m5_lab_2_4.py
@@ -1,7 +1,8 @@
 import argparse
+from ast import literal_eval
 
 
-def flatten_it(iterable_object=[1, 2, 3, ["haha", [3], 7], (9)]):
+def flatten_it(iterable_object=[1, 2, 3, ['haha', [3], 7], (9)]):
     try:
         for x in iterable_object:
             if x == iterable_object:
@@ -19,11 +20,11 @@ def flatten_it(iterable_object=[1, 2, 3, ["haha", [3], 7], (9)]):
 
 def flatten_it_by_args():
     parser = argparse.ArgumentParser(description="Object flattening")  # Creating of parser to use with command line
-    parser.add_argument("-iterable_object", "--iterable_object", action='append',
-                        default=[1, 2, 3, ["haha", [3], 7], (9)], help="Flattening object")
+    parser.add_argument("-iterable_object", "--iterable_object", type=str, help="Flattening object")
     args = parser.parse_args()
-    print(list(flatten_it(args.iterable_object))[:-1])
+    iterable_object = literal_eval(args.iterable_object)
+    print(list(flatten_it(iterable_object)))
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     flatten_it_by_args()
